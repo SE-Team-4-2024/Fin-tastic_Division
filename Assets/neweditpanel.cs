08d6a30c1b925a8c9f2550a3dbd862bdf62c32e2
pavan <008455870@coyote.csusb.edu>
2024-04-19 20:25:42 -0700

    dynamic name allocation

--- Assets/neweditpanel.cs
+++ Assets/neweditpanel.cs
@@ -10,48 +10,9 @@ using UnityEngine.UI;
 public class neweditpanel : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-     //disablebutton2();
-    // disablebutton3();
-     //disablebutton4();
-    }
-    public Button Button2;
-      public Button Button1;
-      public Button Button3;
-      public Button Button4;
-      public GameObject Object;
-       public void disablebutton2()
-      {
-        //Button2.gameObject.SetActive(false);
-         
-         
-      }
-      public void enablebutton2()
-      {
-        // Button2.gameObject.SetActive(true);
-      }
-       public void disablebutton3()
-      {
-        //Button3.gameObject.SetActive(false);
-         
-         
-      }
-      public void enablebutton3()
-      {
-        // Button3.gameObject.SetActive(true);
-      }
- public void disablebutton4()
-      {
-        //Button4.gameObject.SetActive(false);
-         
-         
-      }
-      public void enablebutton4()
-      {
-        // Button4.gameObject.SetActive(true);
-      }
-
+    
+      
+ 
 
 public GameObject Edituserpanel;
     public void openedit() 
@@ -66,7 +27,7 @@ public GameObject Edituserpanel;
     public GameObject Userpanel1;
     public void openusername1() 
     {
-        if(Userpanel1!=null)
+        
           Userpanel1.SetActive(true);
     }
     public void exitopenusername1()
@@ -77,10 +38,9 @@ public GameObject Edituserpanel;
       
   
       public Text un1;
-      public Text un2;
-      public Text un3;
-      
-      public Text un4;
+
+
+     
      
       public void save1()
       {
@@ -88,54 +48,12 @@ public GameObject Edituserpanel;
         username.text="";
        
            }
-           public void save2()
-      {
-         un2.text=username.text;
-       username.text="";
-       
-           }
+          
       
-       public void save3(){
-
-           un3.text=username.text;
-           username.text="";
-       }
-        public void save4(){
-
-           un4.text=username.text;
-           username.text="";
-       }
-       public GameObject Userpanel2;
-         public void openusername2() 
-    {
-        if(Userpanel2!=null)
-          Userpanel2.SetActive(true);
-    }
-    public void exitopenusername2()
-    {
-         Userpanel2.SetActive(false);
-    }  
-    public GameObject Userpanel3;
-         public void openusername3() 
-    {
-        if(Userpanel3!=null)
-          Userpanel3.SetActive(true);
-    }
-    public void exitopenusername3()
-    {
-         Userpanel3.SetActive(false);
-    }  
-    public GameObject Userpanel4;
-         public void openusername4() 
-    {
-        if(Userpanel4!=null)
-          Userpanel4.SetActive(true);
-    }
-    public void exitopenusername4()
-    {
-         Userpanel4.SetActive(false);
-    }  
-    public GameObject Settingspanel;
+      
+        
+          
+       public GameObject Settingspanel;
          public void opensettings() 
     {
         if(Settingspanel!=null)
@@ -164,37 +82,32 @@ public void Switch()
         username.text="";
          
       }
-    else
-    {
-       if(un2.text =="")
-       {
-       un2.text=username.text;
-       username.text="";
-         enablebutton2();
-       }
-        else
-      {
-        if(un3.text =="")
-       { 
-          un3.text=username.text;
-          username.text="";
-           enablebutton3();
+    //else
+    //{
+      // if(un2.text =="")
+       //{
+       //un2.text=username.text;
+       //username.text="";
+       //  enablebutton2();
+      // }
+       // else
+      //{
+        //if(//un3.text =="")
+      // { 
+          //un3.text=username.text;
+         // username.text="";
+         //  enablebutton3();
 
-        }
-        else
-       {
-        un4.text=username.text;
-        username.text=""; 
-        enablebutton4();
-       }
-      }
-}
+       // }
+       
+     // }
+//}
 }
                public Text hello;
-               public Toggle Toggle3;
-       public Toggle Toggle2;
+               
+       
        public Toggle Toggle1;
-       public Toggle Toggle4;
+      
 
 
      
@@ -203,34 +116,72 @@ public void Switch()
         if(Toggle1.isOn)
         {
           hello.text=un1.text;
-         // Toggle2.interactable=false;
-          //Toggle3.interactable=false;
-          //Toggle4.interactable=false;
+         
 
         }
           else{
                
                hello.text="";
-               // Toggle2.interactable=true;
-         // Toggle3.interactable=true;
-         // Toggle4.interactable=true;
+               
            }
           
     }
     
      
      public Transform panelparent;
+     
 
     public GameObject create;
+    
     
 
     public void createpanels()
     {
       
-      {
-        GameObject newpanel=Instantiate(create,panelparent);
+      
+         GameObject newpanel=Instantiate(create,panelparent);
+      newpanel.GetComponentInChildren<Text>().text =Newuserpanel.GetComponentInChildren<InputField>().text;
+      // newpanel.GetComponentInChildren<Text>().text = Userpanel1.GetComponentInChildren<InputField>().text;
+       Newuserpanel.GetComponentInChildren<InputField>().text="";
+    }
+     public void ShowDetails(GameObject newpanel)
+     {
+        newpanel.GetComponentInChildren<Text>().text = Newuserpanel.GetComponentInChildren<InputField>().text;
+       //Newuserpanel.GetComponentInChildren<InputField>().text="";
       }
-    } 
+     public void ShowDetails1(GameObject newpanel)
+     {
+       newpanel.GetComponentInChildren<Text>().text = Userpanel1.GetComponentInChildren<InputField>().text;
+       Userpanel1.GetComponentInChildren<InputField>().text="";
+     }
+     //  if(newpanel.GetComponentInChildren<Text>().text=="") {
+     //   newpanel.GetComponentInChildren<Text>().text = Newuserpanel.GetComponentInChildren<InputField>().text;
+     //   Newuserpanel.GetComponentInChildren<InputField>().text="";
+     //  }
 
+           
+       //    else{
+       // newpanel.GetComponentInChildren<Text>().text = Userpanel1.GetComponentInChildren<InputField>().text;
+       // Userpanel1.GetComponentInChildren<InputField>().text="";
 
-}
+    //  }
+   // } 
+   // }
+        //public Text someTextComponent;   
+        // public InputField someInputField;
+       
+      // public void press(){
+      //   GameObject textObject = GameObject.Find("user1");
+       //  GameObject inputFieldObject = GameObject.Find("userpanel1 (1)");
+      // Text textComponent = textObject.GetComponentInChildren<Text>();
+      // InputField inputField = inputFieldObject.GetComponentInChildren<InputField>();
+        
+      //  textComponent.text = inputField.text;
+        }
+//public GameObject textprefab;
+ //  public void textcreate()
+   //   {
+      //   GameObject newtext=Instantiate(textprefab,transform);
+      //  newtext.GetComponentInChildren<Text>().text =Userpanel1.GetComponentInChildren<InputField>().text;
+      // }
+//}
